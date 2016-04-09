e722a389257c6f189a5442164a2a387f9702be9b
Joshua Hull <joshbuddy@gmail.com>
2016-04-09 00:19:48 -0700

    More progress

--- init.sql
+++ init.sql
@@ -1,17 +1,17 @@
 drop table users;
 drop table games;
 drop table game_players;
-drop table game_data;
 
 create table if not exists users (
   id serial primary key,
   name varchar(20) NOT NULL,
-  password varchar(20) NOT NULL
+  password varchar(100) NOT NULL
 );
-create index on users (name);
+create unique index on users (name);
 
 create table if not exists games (
   id serial primary key,
+  state json NOT NULL,
   finished boolean default TRUE NOT NULL
 );
 create index on games (finished);
@@ -25,13 +25,6 @@ create table if not exists game_players (
 create index on game_players (user_id);
 create index on game_players (game_id);
 create unique index on game_players (user_id, game_id, position);
-
-create table if not exists game_data (
-  id serial primary key,
-  game_id int NOT NULL,
-  value json NOT NULL
-);
-create index on game_data (game_id);
 
 insert into users (name, password) VALUES ('josh', 'rasputin');
 insert into users (name, password) VALUES ('ella', 'rasputin');

872617a43f928bb2de9b1f7050ef8b348b9824e7
Joshua Hull <joshbuddy@gmail.com>
2016-04-11 15:23:53 -0700

    Fix login

--- test/game_test.js
+++ test/game_test.js
@@ -1,5 +1,6 @@
 var request = require('supertest')
 var Server = require('../lib/Server');
+var assert = require('assert');
 
 describe('game', function() {
   beforeEach(function() {
@@ -36,4 +37,33 @@ describe('game', function() {
     })
   })
 
+  describe('POST /login', function() {
+    beforeEach(function(done) {
+      var app = this.app;
+      request(app)
+        .post('/users')
+        .send({name: 'joshbuddy', password: 'password'})
+        .expect(201, 'user created', done);
+    })
+
+    it("should allow login", function(done) {
+        var app = this.app;
+        request(app)
+          .post('/login')
+          .send({name: 'joshbuddy', password: 'password'})
+          .expect(200, 'authorized', function(err, response) {
+            assert(!err, String(err));
+            assert(response.headers['set-cookie'], 'no cookie set');
+            done()
+          });
+    })
+
+    it("should reject an incorrect password", function(done) {
+        var app = this.app;
+        request(app)
+          .post('/login')
+          .send({name: 'joshbuddy', password: 'something'})
+          .expect(401, 'unauthorized', done);
+    })
+  });
 });
